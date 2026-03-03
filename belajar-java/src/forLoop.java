fbbe37ddb49f0f961133199bea24f940238572f2
Rohmat <rohmathidayatyusuf@gmail.com>
2026-03-03 07:44:32 +0700

    new file:   belajar-java/src/array.java 	modified:   belajar-java/src/forLoop.java 	new file:   tugas/10_unggahDanTampilkanGambar.html 	modified:   tugas/9_inputDataPegawai.html

--- belajar-java/src/forLoop.java
+++ belajar-java/src/forLoop.java
@@ -1,19 +1,31 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 public class forLoop {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Input The Number Between 1 - 1000 :");
-        int value = input.nextInt();
-        int n = 1000;
-
-        for (int i = 0; i < n; i++) {
-            if (i == value) {
-                System.out.println("nilai : " + value);
-                break;
-            }
+    public static int calculateNaturalInt (int n){
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
         }
-
-        input.close();
+        return sum;
     }
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+
+        // System.out.print("Input The Number Between 1 - 1000 :");
+        // int value = input.nextInt();
+        // int n = 1000;
+
+        // for (int i = 0; i < n; i++) {
+        //     if (i == value) {
+        //         System.out.println("nilai : " + value);
+        //         break;
+        //     }
+        // }
+
+        // input.close();
+
+        int NaturalInt = calculateNaturalInt(5);
+        System.out.println(NaturalInt);
+    }
+    
 }

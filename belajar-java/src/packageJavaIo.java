05983122b08e1ab49924c98675afa4e3e925c642
Rohmat <rohmathidayatyusuf@gmail.com>
2026-03-04 00:15:05 +0700

    new file:   belajar-java/bin/App.class

--- belajar-java/src/packageJavaIo.java
+++ belajar-java/src/packageJavaIo.java
@@ -1,12 +1,8 @@
-import java.io.File;
+
 public class packageJavaIo {
     public static void main(String[] args) {
-       File test = new File("belajar-java/src/data.txt");
-       
-       if (test.exists()) {
-        System.out.println("File ada");
-       }else{
-        System.out.println("File tidak ada");
-       }
-    }  
+        if (true) {
+            System.out.println("Rumus pythagoras dari 6 dan 8 = " + math.Pythagoras(6, 8));
+        }
+    }
 }

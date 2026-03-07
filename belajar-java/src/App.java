92b34580c9496ce6526a4fde9ec1355dbf1a6227
Rohmat <rohmathidayatyusuf@gmail.com>
2026-03-07 18:39:18 +0700

    modified:   belajar-java/bin/App.class 	new file:   belajar-java/bin/book.class 	modified:   belajar-java/src/App.java 	new file:   belajar-java/src/book.java

--- belajar-java/src/App.java
+++ belajar-java/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        book buku1 = new book("pemrograman dasar", "rohmat", 50000);
+        book buku2 = new book();
+
+        buku1.TampilkanInfo();
+
+        buku1.diskon(50);
+
+        buku1.TampilkanInfo();
+
+        buku2.harga = 10000;
+        buku2.TampilkanInfo();
     }
 }

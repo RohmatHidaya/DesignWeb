b6319b7f5d942f176b9622d562198c4323b1b80a
Rohmat <rohmathidayatyusuf@gmail.com>
2026-03-08 08:29:36 +0700

    test

--- belajar-java/src/gui.java
+++ belajar-java/src/gui.java
@@ -13,22 +13,31 @@ public class gui {
         JTextField text = new JTextField();
         text.setBounds(50, 50, 200, 30);
 
-        JButton button = new JButton("Submit");
-        button.setBounds(50, 100, 100, 30);
+        JButton buttonSubmit = new JButton("Submit");
+        buttonSubmit.setBounds(50, 100, 100, 30);
 
         JLabel tampil = new JLabel();
         tampil.setBounds(50, 125, 200, 30);
 
-        button.addActionListener(new ActionListener() {
+        JButton tombol1 = new JButton("1");
+        tombol1.setBounds(50, 150, 50, 30);
+
+        JButton tombol2 = new JButton("2");
+        tombol2.setBounds(100, 150, 50, 30);
+
+        buttonSubmit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 tampil.setText("halo " + text.getText());
+                int a = 1 + 1;
             }
         });
 
         frame.add(label);
         frame.add(text);
-        frame.add(button);
+        frame.add(buttonSubmit);
         frame.add(tampil);
+        frame.add(tombol1);
+        frame.add(tombol2);
 
         frame.setSize(300, 300);
         frame.setLayout(null);

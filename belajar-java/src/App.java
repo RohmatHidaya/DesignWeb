5e612f14cb03905824a67dfb72ca8e562833a09f
Rohmat <rohmathidayatyusuf@gmail.com>
2026-03-08 09:10:05 +0700

    latihan string

--- belajar-java/src/App.java
+++ belajar-java/src/App.java
@@ -1,17 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
-        book buku1 = new book("pemrograman dasar", "rohmat", 50000);
-        book buku2 = new book();
+        // book buku1 = new book("pemrograman dasar", "rohmat", 50000);
+        // book buku2 = new book();
 
-        buku1.TampilkanInfo();
+        // buku1.TampilkanInfo();
 
-        buku1.diskon(50);
+        // buku1.diskon(50);
 
-        buku1.TampilkanInfo();
+        // buku1.TampilkanInfo();
 
-        buku2.harga = 10000;
-        buku2.TampilkanInfo();
+        // buku2.harga = 10000;
+        // buku2.TampilkanInfo();
+
+        System.out.println(math.sum(1, 1));
     }
 }
